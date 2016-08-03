2dec5fdbcc064e3d1f37f75bead1d19c51daafd9
kristyj <kristypjames@gmail.com>
2016-08-03 16:34:17 +0200

    Move file saving with sys.argv into a function for reusability

--- src/blend/scripts/asset_manipulation_example.py
+++ src/blend/scripts/asset_manipulation_example.py
@@ -5,9 +5,12 @@ def example_manipulation():
     bpy.data.objects['Cube'].rotation_euler = [0.5] * 3
     bpy.data.objects['Cube'].location.x +=1
 
-    print(sys.argv)
-    outputfilename = sys.argv[sys.argv.index('--') + 1]  # Must give filename as first argument after argument '--'
+def save_blend_to_dest(override=None):
+    # Must give filename as first argument after argument '--' if using sys.argv procedure
+    outputfilename = sys.argv[sys.argv.index('--') + 1] if override is None else override
     bpy.ops.wm.save_as_mainfile(filepath=outputfilename, compress=True)
 
 if __name__=="__main__":
     example_manipulation()
+    save_blend_to_dest()
+
