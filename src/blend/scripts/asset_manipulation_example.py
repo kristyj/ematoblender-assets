fc5548da15ec1aec32b15e9fe6c8ba63c52e3216
kristyj <kristypjames@gmail.com>
2016-08-03 15:01:37 +0200

    Move code into a function, so we can test running as a script and importing the function from each gradle task\'s script attribute.

--- src/blend/scripts/asset_manipulation_example.py
+++ src/blend/scripts/asset_manipulation_example.py
@@ -1,9 +1,13 @@
 import bpy
 import sys
 
-bpy.data.objects['Cube'].rotation_euler = [0.5] * 3
-bpy.data.objects['Cube'].location.x +=1
+def example_manipulation():
+    bpy.data.objects['Cube'].rotation_euler = [0.5] * 3
+    bpy.data.objects['Cube'].location.x +=1
 
-print(sys.argv)
-outputfilename = sys.argv[sys.argv.index('--') + 1]  # Must give filename as first argument after argument '--'
-bpy.ops.wm.save_as_mainfile(filepath=outputfilename, compress=True)
+    print(sys.argv)
+    outputfilename = sys.argv[sys.argv.index('--') + 1]  # Must give filename as first argument after argument '--'
+    bpy.ops.wm.save_as_mainfile(filepath=outputfilename, compress=True)
+
+if __name__=="__main__":
+    example_manipulation()
